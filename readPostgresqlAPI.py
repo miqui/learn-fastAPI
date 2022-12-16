3c42b1b0b4c8a098f37068c74ad0ca9bbd7bc4e1
Miguel Quintero <migmaqer@gmail.com>
2022-12-15 21:11:27 -0500

    Create readPostgresqlAPI.py

---
+++ readPostgresqlAPI.py
@@ -0,0 +1,22 @@
+import psycopg2
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Connect to the database
+conn = psycopg2.connect(
+    host="localhost",
+    user="user",
+    password="password",
+    dbname="database"
+)
+
+@app.get("/users")
+def read_users():
+    # Execute a SELECT query to retrieve data from the "users" table
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users")
+    rows = cur.fetchall()
+
+    # Return the retrieved data as a list of dictionaries
+    return [{"id": row[0], "name": row[1]} for row in rows]
